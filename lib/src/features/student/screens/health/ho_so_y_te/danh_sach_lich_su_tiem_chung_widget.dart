449176dba20c586146662d864d55e3d2b53a8c03
Thanmua <nguyenthihongtham1542003@gmail.com>
2024-11-02 21:31:31 +0700

    edit danh_sach_lich_su_tiem_chung_widget.dart

--- lib/src/features/student/screens/health/ho_so_y_te/danh_sach_lich_su_tiem_chung_widget.dart
+++ lib/src/features/student/screens/health/ho_so_y_te/danh_sach_lich_su_tiem_chung_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../../../../../constants/text_strings.dart';
 import '../../../controllers/thong_tin_suc_khoe_controller.dart';
 
 class DanhSachLichSuTiemChungWidget extends StatelessWidget {
@@ -9,12 +10,13 @@ class DanhSachLichSuTiemChungWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView( // Wrap in SingleChildScrollView
+    const buttonColor = Color(0xFF03045E);
+    return SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const Text(
-            'Lịch sử tiêm chủng',
+            tLichSuTiemChungHeader ,
             style: TextStyle(
               color: Color(0xFF7B7B7B),
               fontWeight: FontWeight.bold,
@@ -23,21 +25,19 @@ class DanhSachLichSuTiemChungWidget extends StatelessWidget {
           ),
           const SizedBox(height: 16),
           Obx(() {
-            // Ensure selectedVaccine.value is not null before checking isNotEmpty
             if (controller.isDetailView.value &&
-                controller.selectedVaccine.value?.isNotEmpty == true) {
-              final selectedVaccineInfo = controller.vaccineHistory.firstWhere(
-                    (vaccine) =>
-                vaccine['LoaiVaccine'] == controller.selectedVaccine.value,
-                orElse: () => {},
-              );
-              final vaccineHistoryInfo = selectedVaccineInfo['vaccineHistory_id_1_12'] ??
-                  {};
+                controller.selectedVaccine.value.isNotEmpty) {
+              final vaccineHistoryId = controller.selectedVaccine.value;
+              final selectedVaccineHistory =
+                  controller.vaccineHistory[vaccineHistoryId] ?? {};
+              final vaccineId = selectedVaccineHistory["vaccineID"];
+              final vaccineInfo = controller.vaccineData[vaccineId] ?? {};
+
               return Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'Chi tiết về ${controller.selectedVaccine.value}',
+                    '$tChiTietVe ${vaccineInfo['vaccineName']} \n ${vaccineInfo['description']}',
                     style: const TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
@@ -47,57 +47,61 @@ class DanhSachLichSuTiemChungWidget extends StatelessWidget {
                   ListView.builder(
                     shrinkWrap: true,
                     physics: const NeverScrollableScrollPhysics(),
-                    itemCount: vaccineHistoryInfo['doses']?.length ?? 0,
+                    itemCount: selectedVaccineHistory['doses']?.length ?? 0,
                     itemBuilder: (context, index) {
-                      final vaccineInfo = vaccineHistoryInfo['doses']?[index];
+                      final doseInfo = selectedVaccineHistory['doses'][index];
                       return Card(
-                        color: index.isEven ? Colors.white : const Color(
-                            0xFFE9EFF7),
+                        color: index.isEven
+                            ? Colors.white
+                            : const Color(0xFFE9EFF7),
+                        shadowColor: Colors.black,
+                        elevation: 4.0,
                         child: Padding(
                           padding: const EdgeInsets.all(16.0),
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
-                                'Liều tiêm: ${vaccineInfo?['dose'] ?? 'N/A'}',
+                                '$tLieu ${index + 1} ',
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                '$tNgayTiem ${doseInfo['dateAdministered']}',
+                                style: const TextStyle(fontSize: 16),
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                '$tLieuLuongVaccine ${doseInfo['dose']}',
                                 style: const TextStyle(
                                   fontSize: 16,
-                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
                               const SizedBox(height: 8),
                               Text(
-                                'Ngày tiêm: ${vaccineInfo?['dateAdministered'] ??
-                                    'N/A'}',
+                                '$tViTriTiem ${doseInfo['site']}',
                                 style: const TextStyle(fontSize: 16),
                               ),
                               const SizedBox(height: 8),
                               Text(
-                                'Vị trí tiêm: ${vaccineInfo?['site'] ?? 'N/A'}',
+                                '$tNhaCungCap ${doseInfo['provider']}',
                                 style: const TextStyle(fontSize: 16),
                               ),
                               const SizedBox(height: 8),
                               Text(
-                                'Nơi tiêm: ${vaccineInfo?['provider'] ??
-                                    'N/A'}',
+                                '$tPhanUngSauTiem ${doseInfo['sideEffects']}',
                                 style: const TextStyle(fontSize: 16),
                               ),
                               const SizedBox(height: 8),
                               Text(
-                                'Phản ứng sau tiêm: ${vaccineInfo?['sideEffects'] ??
-                                    'N/A'}',
+                                '$tLieuTiepTheo ${doseInfo['nextDoseDue']}',
                                 style: const TextStyle(fontSize: 16),
                               ),
                               const SizedBox(height: 8),
                               Text(
-                                'Liều tiếp theo: ${vaccineInfo?['nextDoseDue'] ??
-                                    'N/A'}',
-                                style: const TextStyle(fontSize: 16),
-                              ),
-                              const SizedBox(height: 8),
-                              Text(
-                                'Tiến độ tiêm: ${vaccineInfo?['vaccinationProgress'] ??
-                                    'N/A'}',
+                                '$tTienTrinhTiem ${doseInfo['vaccinationProgress']}',
                                 style: const TextStyle(fontSize: 16),
                               ),
                             ],
@@ -108,11 +112,16 @@ class DanhSachLichSuTiemChungWidget extends StatelessWidget {
                   ),
                   const SizedBox(height: 16),
                   ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white, backgroundColor: buttonColor, // Màu chữ của Button
+                      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                      textStyle: const TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+                    ),
                     onPressed: () {
                       controller.isDetailView.value = false;
                       controller.selectedVaccine.value = '';
                     },
-                    child: const Text('Quay lại danh sách'),
+                    child: const Text(tQuayLaiDanhSach),
                   ),
                 ],
               );
@@ -120,42 +129,62 @@ class DanhSachLichSuTiemChungWidget extends StatelessWidget {
               return SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: DataTable(
-                  headingRowColor: WidgetStateProperty.all(
-                      const Color(0xFFCAF0F8)),
+                  headingRowColor:
+                      WidgetStateProperty.all(const Color(0xFFCAF0F8)),
                   columns: const [
-                    DataColumn(label: Text(
-                        'STT', style: TextStyle(fontWeight: FontWeight.bold,
-                        fontSize: 18))),
-                    DataColumn(label: Text(
-                        'Loại vaccine', style: TextStyle(fontWeight: FontWeight
-                        .bold, fontSize: 18))),
-                    DataColumn(label: Text(
-                        'Chi tiết', style: TextStyle(fontWeight: FontWeight
-                        .bold, fontSize: 18))),
+                    DataColumn(
+                      label: Text(
+                        tSTTTableColumn,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        tLoaiVaccineTableColumn,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                    ),
+                    DataColumn(
+                      label: Text(
+                        tChiTietTableColumn,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                    ),
                   ],
                   rows: List<DataRow>.generate(
-                    controller.vaccineHistory.length,
-                        (index) {
-                      final vaccine = controller.vaccineHistory[index];
-                      final rowColor = index.isEven
-                          ? Colors.white
-                          : const Color(0xFFE9EFF7);
+                    controller.selectedVaccineHistory.length,
+                    (index) {
+                      final vaccineHistory =
+                          controller.selectedVaccineHistory[index];
+                      final rowColor =
+                          index.isEven ? Colors.white : const Color(0xFFE9EFF7);
                       return DataRow(
                         color: WidgetStateProperty.all(rowColor),
                         cells: [
-                          DataCell(Text(vaccine['STT'] ?? '',
-                              style: const TextStyle(fontSize: 18))),
-                          DataCell(Text(vaccine['LoaiVaccine'] ?? '',
-                              style: const TextStyle(fontSize: 18))),
+                          DataCell(
+                            Text(
+                              (index + 1).toString(),
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                          ),
+                          DataCell(
+                            Text(
+                              vaccineHistory['LoaiVaccine'] ?? '',
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                          ),
                           DataCell(
                             TextButton(
                               onPressed: () {
                                 controller.isDetailView.value = true;
                                 controller.selectedVaccine.value =
-                                vaccine['LoaiVaccine'];
+                                    vaccineHistory['vaccineHistoryId'];
                               },
                               child: const Text(
-                                'Xem chi tiết',
+                                tXemChiTietButton,
                                 style: TextStyle(
                                   decoration: TextDecoration.underline,
                                   color: Color(0xFF03045E),
